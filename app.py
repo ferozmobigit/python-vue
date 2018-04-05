6bc68f2591658fb2da5bde47e4a4a74318ca7c13
Feroz Shaik <feroz.shaik@ggktech.com>
2018-04-05 16:38:08 +0530

    ferozmobigit

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return render_template('home.html')
 
+@app.route('/signup')
+def signUp():
+    return render_template('signup.html')
+    
+# @app.route('/details')
+def details():
+    return "flask for back end and vue.js for front end."
+
+app.add_url_rule('/details', 'details', details)
 if __name__ == '__main__':
-    app.run()
+    # app.run(host, port, debug, options)
+    app.run(host='0.0.0.0', port=1210)
